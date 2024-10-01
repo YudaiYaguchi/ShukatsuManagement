c90478904d4b2aee09e6a4421a09de49ce2ceb49
YudaiYaguchi <yudai.yaguchi0503@gmail.com>
2024-10-02 01:08:22 +0900

    fix:server.jsの修正

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// 静的ファイルの提供
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// 任意のルートがアクセスされた場合、index.htmlを返す
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// サーバーのポート設定
+const PORT = process.env.PORT || 5000;
+
+// サーバーを起動
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
